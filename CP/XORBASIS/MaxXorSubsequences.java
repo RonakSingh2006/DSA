a415a9afc4d7d557c08adaa65b5727267e359c6d
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-09-20 22:26:11 +0530

    Learned XOR Basis Cp concept

---
+++ CP/XORBASIS/MaxXorSubsequences.java
@@ -0,0 +1,33 @@
+package CP.XORBASIS;
+
+class Solution {
+    public int maxXorSubsequences(int[] nums) {
+        // find max subseq XOR
+        int bases[] = new int[32];
+        for(int x : nums){
+            for(int i=31 ; i>=0 ; i--){
+                if((x & (1<<i)) != 0){
+                    if(bases[i] != 0){
+                        x ^= bases[i];
+                    }
+                    else{
+                        bases[i] = x;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // now we have all dominant terms
+
+        int ans = 0;
+
+        for(int i=31 ; i>=0 ; i--){
+            if((bases[i] & (1<<i)) != 0){
+                ans ^= bases[i];
+            }
+        }
+
+        return ans;
+    }
+}
