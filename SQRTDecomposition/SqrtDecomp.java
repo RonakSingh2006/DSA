6ec0162f62924daef15ec9e53ae0f0f69e98e9c7
RonakSingh2006 <ronaksingh201106@gmail.com>
2026-03-06 23:15:24 +0530

    final class based solution

--- SQRTDecomposition/SqrtDecomp.java
+++ SQRTDecomposition/SqrtDecomp.java
@@ -1,56 +1,61 @@
 // Use to get the range query
 
 public class SqrtDecomp{
-  public static void main(String[] args) {
-    int arr[] = {5,6,7,8,9,10,15};
 
-    int n = arr.length;
+  int arr[];
+  int sqrt , n;
+  int blocks[];
 
-    // we make a array of size Math.ceil(sqrt)
+  SqrtDecomp(int arr[]){
+    this.arr = arr;
+    this.n = arr.length;
+    this.sqrt = (int)Math.ceil(Math.sqrt(n));
+    int len = sqrt + 1;
+    this.blocks = new int[len];
 
-    int sqrt = (int)Math.sqrt(n);
+    fill();
+  }
 
-    int len = (n/sqrt) + 1;
-
-    int block[] = new int[len];
-
-    // we store sum in range of sqrt
+  private void fill(){
     for(int i=0 ; i<n ; i++){
-      block[i/sqrt] += arr[i];
+      blocks[i/sqrt] += arr[i];
+    }
+  }
+
+  public int query(int l , int r){
+    int sum = 0;
+    // first part until we dont reach at block edge
+
+
+    while(l<=r && l%sqrt != 0){
+      sum += arr[l];
+      l++;
     }
 
-    int queries[][] = {{1,2},{5,6},{1,6},{0,4},{2,5}};
+    // middle part we take blocks that come in range
 
-
-    for(int q[] : queries){
-      int l = q[0];
-      int r = q[1];
-
-      // first part until we dont reach at block edge
-
-      int sum = 0;
-
-      while(l<=r && l%sqrt != 0){
-        sum += arr[l];
-        l++;
-      }
-
-      // middle part we take blocks that come in range
-
-      while(l + sqrt <= r){
-        sum += block[l/sqrt];
-        l+= sqrt;
-      }
-
-      // end part
-
-      while(l<=r){
-        sum += arr[l];
-        l++;
-      }
-
-
-      System.out.println(sum);
+    while(l + sqrt <= r){
+      sum += blocks[l/sqrt];
+      l+= sqrt;
     }
+
+    // end part
+
+    while(l<=r){
+      sum += arr[l];
+      l++;
+    }
+
+    return sum;
+
+  }
+
+  public void update(int i , int val){
+    // update block
+
+    blocks[i/sqrt] += (val - arr[i]);
+
+    arr[i] = val;
+
   }
 }
